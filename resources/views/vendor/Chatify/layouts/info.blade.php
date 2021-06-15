71a10b95d5f3daa9f8fffa8e09506211aeed37ab
Amrit Sinha <purpleplots@gmail.com>
2021-06-15 13:03:06 +0530

    Inbox UI Improvements

--- resources/views/vendor/Chatify/layouts/info.blade.php
+++ resources/views/vendor/Chatify/layouts/info.blade.php
@@ -1,5 +1,6 @@
 {{-- user info and avatar --}}
-<div class="avatar av-l"></div>
+{{-- <div class="avatar av-l"></div> --}}
+{{-- {{ $user->profile_photo_url }} --}}
 <p class="info-name">{{ config('chatify.name') }}</p>
 <div class="messenger-infoView-btns">
     {{-- <a href="#" class="default"><i class="fas fa-camera"></i> default</a> --}}
--- resources/views/vendor/Chatify/pages/app.blade.php
+++ resources/views/vendor/Chatify/pages/app.blade.php
@@ -65,8 +65,8 @@
                 {{-- header back button, avatar and user name --}}
                 <div style="display: inline-flex;">
                     <a href="#" class="show-listView"><i class="fas fa-arrow-left"></i></a>
-                    <div class="avatar av-s header-avatar" style="margin: 0px 10px; margin-top: -5px; margin-bottom: -5px;">
-                    </div>
+                    {{-- <div class="avatar av-s header-avatar" style="background-image: url('{{ $user->profile_photo_url }}');">
+                    </div> --}}
                     <a href="#" class="user-name">{{ config('chatify.name') }}</a>
                 </div>
                 {{-- header buttons --}}
