8de7928ccce8e9a4bb9a09010c18ebd1cb87b291
Amrit Sinha <purpleplots@gmail.com>
2021-06-15 16:17:50 +0530

    bu Fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\View\Components\Navbar;
+use App\View\Components\Sidebar;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Schema;
@@ -27,8 +29,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        Blade::component('sidebar', Sidebar::class);
-        Blade::component('navbar', Navbar::class);
+        // Blade::component('sidebar', Sidebar::class);
+        // Blade::component('navbar', Navbar::class);
         
         Gate::define('isAdmin', function($user)
         {
