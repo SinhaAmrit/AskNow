7c114f5a23b9c740a179ea8f26d6011996a84d37
Amrit Sinha <purpleplots@gmail.com>
2021-06-18 20:25:40 +0530

    Home Styling Done

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -11,3 +11,6 @@
         </div>
     </div>
 </x-guest-layout>
+{{-- <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+</div> --}}
